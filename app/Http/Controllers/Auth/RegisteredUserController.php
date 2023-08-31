0ccaadb92b88cec039f6d950445dd4a26d954f07
Zabir Emu <zabirraihan570@gmail.com>
2023-08-31 13:48:38 +0600

    complete laravel react backend authentication

---
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $codeValidation = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required'],
+        ]);
+
+        if ($codeValidation->fails()) {
+            return response()->json(['errors', $codeValidation->errors()]);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        $token = $user->createToken('api_token');
+
+        return response()->json([
+            'success' => 'Register success',
+            'user' => $user,
+            'api_token' => $token,
+        ]);
+    }
+}
